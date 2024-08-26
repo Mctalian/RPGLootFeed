66b7dab4f339705ae8e4d3927116e6712cb375c4
Rob Anderson <Mctalian@users.noreply.github.com>
2024-08-26 15:15:15 -0600

    Hopeful fix

--- spec/Features/Currency_spec.lua
+++ spec/Features/Currency_spec.lua
@@ -15,9 +15,7 @@ describe("Currency module", function()
       }
 
       _G.C_CurrencyInfo = {
-        GetCurrencyInfo = function()
-          print("Original")
-        end,
+        GetCurrencyInfo = function() end,
         GetCurrencyLink = function()
             return "|c12345678|Hcurrency:123|r"
         end
@@ -35,7 +33,7 @@ describe("Currency module", function()
 
       _G.G_RLF.Currency:OnUpdate(123, 1, 1)
 
-      assert.stub(mockLootDisplay.ShowLoot).was.not_called()
+      assert.stub(G_RLF.LootDisplay.ShowLoot).was.not_called()
   end)
 
   it("does not show loot if the currency type is nil", function()
@@ -43,7 +41,7 @@ describe("Currency module", function()
 
       _G.G_RLF.Currency:OnUpdate(nil)
 
-      assert.stub(mockLootDisplay.ShowLoot).was.not_called()
+      assert.stub(G_RLF.LootDisplay.ShowLoot).was.not_called()
   end)
 
   it("does not show loot if the quantityChange is nil", function()
@@ -51,7 +49,7 @@ describe("Currency module", function()
 
     _G.G_RLF.Currency:OnUpdate(123, nil, nil)
 
-    assert.stub(mockLootDisplay.ShowLoot).was.not_called()
+    assert.stub(G_RLF.LootDisplay.ShowLoot).was.not_called()
 end)
 
   it("does not show loot if the quantityChange is lte 0", function()
@@ -59,26 +57,24 @@ end)
 
       _G.G_RLF.Currency:OnUpdate(123, nil, -1)
 
-      assert.stub(mockLootDisplay.ShowLoot).was.not_called()
+      assert.stub(G_RLF.LootDisplay.ShowLoot).was.not_called()
   end)
 
   it("does not show loot if the currency info cannot be found", function()
       _G.G_RLF.db.global.currencyFeed = true
       _G.C_CurrencyInfo.GetCurrencyInfo = function()
-          print("nilled")
           return nil
       end
 
       _G.G_RLF.Currency:OnUpdate(123, 1, 1)
 
-      assert.stub(mockLootDisplay.ShowLoot).was.not_called()
+      assert.stub(G_RLF.LootDisplay.ShowLoot).was.not_called()
   end)
 
   it("shows loot if the currency info is valid", function()
       _G.G_RLF.db.global.currencyFeed = true
-      mockCurrencyInfo.GetCurrencyInfo:revert()
+
       _G.C_CurrencyInfo.GetCurrencyInfo = function()
-          print("Overridden")
           return {
             currencyID = 123,
             iconFileID = 123456
@@ -87,6 +83,6 @@ end)
 
       _G.G_RLF.Currency:OnUpdate(123, 5, 2)
 
-      assert.stub(mockLootDisplay.ShowLoot).was.called()
+      assert.stub(G_RLF.LootDisplay.ShowLoot).was.called_with(_, 123, "|c12345678|Hcurrency:123|r", 123456, 2)
   end)
 end)
