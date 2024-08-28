47372a63860a96d6bb144b33f5722afc25890e09
Rob Anderson <mctalian@no-reply.github.com>
2024-08-28 16:11:57 -0600

    Investigate major faction rep issue

--- Features/Reputation.lua
+++ Features/Reputation.lua
@@ -4,7 +4,7 @@ local repData = {}
 local paragonRepData = {}
 local majorRepData = {}
 local cachedFactionCount
-function Rep:RefreshRepData()
+function Rep:Snapshot()
 	C_Reputation.ExpandAllFactionHeaders()
 	local numFactions = C_Reputation.GetNumFactions()
 	if numFactions <= 0 then
@@ -34,8 +34,25 @@ function Rep:RefreshRepData()
 			end
 		end
 	end
+	G_RLF:Print("Rep refreshed")
+	G_RLF:Print(dump(majorRepData[2590]))
 
 	cachedFactionCount = count
+end
+
+function dump(o)
+	if type(o) == "table" then
+		local s = "{ "
+		for k, v in pairs(o) do
+			if type(k) ~= "number" then
+				k = '"' .. k .. '"'
+			end
+			s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+		end
+		return s .. "} "
+	else
+		return tostring(o)
+	end
 end
 
 function Rep:AddAnyNewFactions()
@@ -55,6 +72,7 @@ function Rep:AddAnyNewFactions()
 				if C_Reputation.IsFactionParagon(fId) then
 					if not paragonRepData[fId] then
 						paragonRepData[fId] = 0
+						G_RLF:Print(fId .. " added")
 					end
 				elseif C_Reputation.IsMajorFaction(fId) then
 					if not majorRepData[fId] then
@@ -62,15 +80,18 @@ function Rep:AddAnyNewFactions()
 						local level = mfd.renownLevel
 						local max = mfd.renownLevelThreshold
 						majorRepData[fId] = { level, 0, max }
+						G_RLF:Print(fId .. " added")
 					end
 				else
 					if not repData[fId] then
 						repData[fId] = 0
+						G_RLF:Print(fId .. " added")
 					end
 				end
 			end
 		end
 	end
+	G_RLF:Print("New factions added")
 end
 
 function Rep:FindDelta()
@@ -82,6 +103,7 @@ function Rep:FindDelta()
 			local factionData = C_Reputation.GetFactionDataByID(k)
 			if factionData.currentStanding ~= v then
 				G_RLF.LootDisplay:ShowRep(factionData.currentStanding - v, factionData)
+				repData[k] = factionData.currentStanding
 			end
 		end
 		-- Paragon facions
@@ -96,27 +118,66 @@ function Rep:FindDelta()
 				else
 					G_RLF.LootDisplay:ShowRep(value - v, factionData)
 				end
+				paragonRepData[k] = value
 			end
 		end
 		-- Major factions
 		for k, v in pairs(majorRepData) do
 			local factionData = C_Reputation.GetFactionDataByID(k)
 			local majorFactionData = C_MajorFactions.GetMajorFactionData(k)
-			local level = majorFactionData.renownLevel
+			local level = C_MajorFactions.GetCurrentRenownLevel(k)
 			local rep = majorFactionData.renownReputationEarned
 			local max = majorFactionData.renownLevelThreshold
 			local oldLevel, oldRep, oldMax = unpack(v)
+			if k == 2590 then
+				G_RLF:Print(dump(v))
+				G_RLF:Print(oldLevel .. "->" .. level)
+				G_RLF:Print(oldRep .. "->" .. rep)
+				G_RLF:Print(oldMax .. "->" .. max)
+			end
 			-- It seems like the renownLevel is not updated quick enough and comes back as the same value
 			-- In that case, we will see the same level, but the rep will drop
 			if rep > oldRep then
 				G_RLF.LootDisplay:ShowRep(rep - oldRep, factionData)
 			elseif rep < oldRep then
 				G_RLF.LootDisplay:ShowRep(oldMax - oldRep + rep, factionData)
+			elseif rep == oldRep and level > oldLevel then
+				G_RLF.LootDisplay:ShowRep(oldMax - oldRep + rep, factionData)
 			end
+			majorRepData[k] = { level, rep, max }
 		end
 	end
 
-	Rep:RefreshRepData()
+	-- Rep:Snapshot()
+end
+
+function handleMajorFactionRepChange(id, level)
+	local factionData = C_Reputation.GetFactionDataByID(id)
+	local majorFactionData = C_MajorFactions.GetMajorFactionData(id)
+	local level = level or C_MajorFactions.GetCurrentRenownLevel(id)
+	local rep = majorFactionData.renownReputationEarned
+	local max = majorFactionData.renownLevelThreshold
+	local oldLevel, oldRep, oldMax = unpack(v)
+	if id == 2590 then
+		G_RLF:Print(dump(v))
+		G_RLF:Print(oldLevel .. "->" .. level)
+		G_RLF:Print(oldRep .. "->" .. rep)
+		G_RLF:Print(oldMax .. "->" .. max)
+	end
+	-- It seems like the renownLevel is not updated quick enough and comes back as the same value
+	-- In that case, we will see the same level, but the rep will drop
+	if rep > oldRep then
+		G_RLF.LootDisplay:ShowRep(rep - oldRep, factionData)
+	elseif rep < oldRep then
+		G_RLF.LootDisplay:ShowRep(oldMax - oldRep + rep, factionData)
+	elseif rep == oldRep and level > oldLevel then
+		G_RLF.LootDisplay:ShowRep(oldMax - oldRep + rep, factionData)
+	end
+	majorRepData[id] = { level, rep, max }
+end
+
+function Rep:OnChangeMajorFactionRenownLevel(mfID, newLevel, oldLevel)
+	handleMajorFactionRepChange(mfID, newLevel)
 end
 
 G_RLF.Rep = Rep
--- spec/Features/Reputation_spec.lua
+++ spec/Features/Reputation_spec.lua
@@ -1,12 +1,77 @@
 describe("Reputation module", function()
 	before_each(function()
 		-- Define the global G_RLF
-		_G.G_RLF = {}
+		_G.unpack = function(t, i)
+			i = i or 1
+			if t[i] ~= nil then
+				return t[i], _G.unpack(t, i + 1)
+			end
+		end
+		_G.G_RLF = {
+			db = {
+				global = {},
+			},
+			LootDisplay = {
+				ShowRep = function() end,
+			},
+			Print = function(_, msg) end,
+		}
+		_G.C_Reputation = {
+			ExpandAllFactionHeaders = function() end,
+			GetFactionDataByID = function()
+				return
+			end,
+			GetFactionParagonInfo = function()
+				return
+			end,
+			GetFactionDataByIndex = function(i)
+				if i == 1 then
+					return {
+						isHeader = true,
+					}
+				elseif i == 2 then
+					return {
+						isHeader = true,
+						isHeaderWithRep = true,
+						factionID = 1,
+					}
+				else
+				end
+				return
+			end,
+			GetNumFactions = function()
+				return 3
+			end,
+			IsFactionParagon = function()
+				return false
+			end,
+			IsMajorFaction = function()
+				return true
+			end,
+		}
+		_G.C_MajorFactions = {
+			GetMajorFactionData = function()
+				return {
+					renownLevel = 1,
+					renownReputationEarned = 150,
+					renownLevelThreshold = 2500,
+				}
+			end,
+		}
 		-- Load the list module before each test
 		dofile("Features/Reputation.lua")
 	end)
 
-	it("TODO", function()
-		assert.are.equal(true, true)
+	it("correctly calculates the rep gain", function()
+		_G.G_RLF.db.global.repFeed = true
+		_G.G_RLF.Rep:FindDelta()
+		_G.C_MajorFactions.GetMajorFactionData = function()
+			return {
+				renownLevel = 1,
+				renownReputationEarned = 300,
+				renownLevelThreshold = 2500,
+			}
+		end
+		_G.G_RLF.Rep:FindDelta()
 	end)
 end)
