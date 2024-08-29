485c3be1b4b7f882068abd5d41dd8eaa1d377628
Rob Anderson <Mctalian@users.noreply.github.com>
2024-08-28 21:50:24 -0600

    Fix #71

--- Features/Reputation.lua
+++ Features/Reputation.lua
@@ -5,93 +5,61 @@ local paragonRepData = {}
 local majorRepData = {}
 local cachedFactionCount
 function Rep:Snapshot()
-	C_Reputation.ExpandAllFactionHeaders()
-	local numFactions = C_Reputation.GetNumFactions()
-	if numFactions <= 0 then
-		return
-	end
-
 	local count = 0
-	for i = 1, numFactions do
-		local factionData = C_Reputation.GetFactionDataByIndex(i)
-		if factionData ~= nil then
-			if not factionData.isHeader or factionData.isHeaderWithRep then
-				if C_Reputation.IsFactionParagon(factionData.factionID) then
-					-- Need to support Paragon factions
-					local value, max = C_Reputation.GetFactionParagonInfo(factionData.factionID)
-					paragonRepData[factionData.factionID] = value
-				elseif C_Reputation.IsMajorFaction(factionData.factionID) then
-					-- Need to support Major factions
-					local majorFactionData = C_MajorFactions.GetMajorFactionData(factionData.factionID)
-					local level = majorFactionData.renownLevel
-					local rep = majorFactionData.renownReputationEarned
-					local max = majorFactionData.renownLevelThreshold
-					majorRepData[factionData.factionID] = { level, rep, max }
-				else
-					repData[factionData.factionID] = factionData.currentStanding
-				end
-				count = count + 1
+	local i = 1
+	local factionData = C_Reputation.GetFactionDataByIndex(i)
+	while factionData ~= nil do
+		if not factionData.isHeader or factionData.isHeaderWithRep then
+			if C_Reputation.IsFactionParagon(factionData.factionID) then
+				-- Need to support Paragon factions
+				local value, max = C_Reputation.GetFactionParagonInfo(factionData.factionID)
+				paragonRepData[factionData.factionID] = value
+			elseif C_Reputation.IsMajorFaction(factionData.factionID) then
+				-- Need to support Major factions
+				local majorFactionData = C_MajorFactions.GetMajorFactionData(factionData.factionID)
+				local level = majorFactionData.renownLevel
+				local rep = majorFactionData.renownReputationEarned
+				local max = majorFactionData.renownLevelThreshold
+				majorRepData[factionData.factionID] = { level, rep, max }
+			else
+				repData[factionData.factionID] = factionData.currentStanding
 			end
+			count = count + 1
 		end
+		i = i + 1
+		factionData = C_Reputation.GetFactionDataByIndex(i)
 	end
-	G_RLF:Print("Rep refreshed")
-	G_RLF:Print(dump(majorRepData[2590]))
 
 	cachedFactionCount = count
 end
 
-function dump(o)
-	if type(o) == "table" then
-		local s = "{ "
-		for k, v in pairs(o) do
-			if type(k) ~= "number" then
-				k = '"' .. k .. '"'
-			end
-			s = s .. "[" .. k .. "] = " .. dump(v) .. ","
-		end
-		return s .. "} "
-	else
-		return tostring(o)
-	end
-end
-
 function Rep:AddAnyNewFactions()
-	-- Unfortunately, everything needs to be expanded before we can get the number of factions
-	C_Reputation.ExpandAllFactionHeaders()
-	local numFactions = C_Reputation.GetNumFactions()
-	if numFactions <= 0 then
-		return
-	end
-
-	for i = 1, numFactions do
-		local factionData = C_Reputation.GetFactionDataByIndex(i)
-		if factionData ~= nil then
-			if not factionData.isHeader or factionData.isHeaderWithRep then
-				local factionData = C_Reputation.GetFactionDataByIndex(i)
-				local fId = factionData.factionID
-				if C_Reputation.IsFactionParagon(fId) then
-					if not paragonRepData[fId] then
-						paragonRepData[fId] = 0
-						G_RLF:Print(fId .. " added")
-					end
-				elseif C_Reputation.IsMajorFaction(fId) then
-					if not majorRepData[fId] then
-						local mfd = C_MajorFactions.GetMajorFactionData(fId)
-						local level = mfd.renownLevel
-						local max = mfd.renownLevelThreshold
-						majorRepData[fId] = { level, 0, max }
-						G_RLF:Print(fId .. " added")
-					end
-				else
-					if not repData[fId] then
-						repData[fId] = 0
-						G_RLF:Print(fId .. " added")
-					end
+	local i = 1
+	local factionData = C_Reputation.GetFactionDataByIndex(i)
+	while factionData ~= nil do
+		if not factionData.isHeader or factionData.isHeaderWithRep then
+			local factionData = C_Reputation.GetFactionDataByIndex(i)
+			local fId = factionData.factionID
+			if C_Reputation.IsFactionParagon(fId) then
+				if not paragonRepData[fId] then
+					paragonRepData[fId] = 0
+				end
+			elseif C_Reputation.IsMajorFaction(fId) then
+				if not majorRepData[fId] then
+					local mfd = C_MajorFactions.GetMajorFactionData(fId)
+					local level = mfd.renownLevel
+					local max = mfd.renownLevelThreshold
+					majorRepData[fId] = { level, 0, max }
+				end
+			else
+				if not repData[fId] then
+					repData[fId] = 0
 				end
 			end
 		end
+		i = i + 1
+		factionData = C_Reputation.GetFactionDataByIndex(i)
 	end
-	G_RLF:Print("New factions added")
 end
 
 function Rep:FindDelta()
@@ -123,32 +91,9 @@ function Rep:FindDelta()
 		end
 		-- Major factions
 		for k, v in pairs(majorRepData) do
-			local factionData = C_Reputation.GetFactionDataByID(k)
-			local majorFactionData = C_MajorFactions.GetMajorFactionData(k)
-			local level = C_MajorFactions.GetCurrentRenownLevel(k)
-			local rep = majorFactionData.renownReputationEarned
-			local max = majorFactionData.renownLevelThreshold
-			local oldLevel, oldRep, oldMax = unpack(v)
-			if k == 2590 then
-				G_RLF:Print(dump(v))
-				G_RLF:Print(oldLevel .. "->" .. level)
-				G_RLF:Print(oldRep .. "->" .. rep)
-				G_RLF:Print(oldMax .. "->" .. max)
-			end
-			-- It seems like the renownLevel is not updated quick enough and comes back as the same value
-			-- In that case, we will see the same level, but the rep will drop
-			if rep > oldRep then
-				G_RLF.LootDisplay:ShowRep(rep - oldRep, factionData)
-			elseif rep < oldRep then
-				G_RLF.LootDisplay:ShowRep(oldMax - oldRep + rep, factionData)
-			elseif rep == oldRep and level > oldLevel then
-				G_RLF.LootDisplay:ShowRep(oldMax - oldRep + rep, factionData)
-			end
-			majorRepData[k] = { level, rep, max }
+			handleMajorFactionRepChange(k)
 		end
 	end
-
-	-- Rep:Snapshot()
 end
 
 function handleMajorFactionRepChange(id, level)
@@ -157,15 +102,8 @@ function handleMajorFactionRepChange(id, level)
 	local level = level or C_MajorFactions.GetCurrentRenownLevel(id)
 	local rep = majorFactionData.renownReputationEarned
 	local max = majorFactionData.renownLevelThreshold
-	local oldLevel, oldRep, oldMax = unpack(v)
-	if id == 2590 then
-		G_RLF:Print(dump(v))
-		G_RLF:Print(oldLevel .. "->" .. level)
-		G_RLF:Print(oldRep .. "->" .. rep)
-		G_RLF:Print(oldMax .. "->" .. max)
-	end
-	-- It seems like the renownLevel is not updated quick enough and comes back as the same value
-	-- In that case, we will see the same level, but the rep will drop
+	local oldLevel, oldRep, oldMax = unpack(majorRepData[id])
+
 	if rep > oldRep then
 		G_RLF.LootDisplay:ShowRep(rep - oldRep, factionData)
 	elseif rep < oldRep then
