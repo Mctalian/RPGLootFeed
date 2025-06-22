2d78ca915a49e76437d3f15350571a30cd229925
Rob Anderson <Mctalian@users.noreply.github.com>
2025-06-22 10:05:34 -0600

    test: add more mocks to C_TransmogCollection

--- RPGLootFeed_spec/_mocks/WoWGlobals/namespaces/C_TransmogCollection.lua
+++ RPGLootFeed_spec/_mocks/WoWGlobals/namespaces/C_TransmogCollection.lua
@@ -6,5 +6,9 @@ local transmogCollectionMocks = {}
 _G.C_TransmogCollection = {}
 transmogCollectionMocks.PlayerHasTransmogByItemInfo =
 	stub(_G.C_TransmogCollection, "PlayerHasTransmogByItemInfo").returns(true)
+transmogCollectionMocks.PlayerHasTransmog = stub(_G.C_TransmogCollection, "PlayerHasTransmog").returns(true)
+transmogCollectionMocks.GetItemInfo = stub(_G.C_TransmogCollection, "GetItemInfo").returns(12345, 1)
+transmogCollectionMocks.GetAppearanceSourceInfo =
+	stub(_G.C_TransmogCollection, "GetAppearanceSourceInfo").returns(0, 1, false, 1, false, "link", "tmogLink", 1, 1)
 
 return transmogCollectionMocks
