a7ff9d87e0a82685c003c460738c78bf6b2299cb
Rob Anderson <Mctalian@users.noreply.github.com>
2025-07-01 17:47:30 -0600

    add reps for new factions in 11.2.0

--- RPGLootFeed_spec/_mocks/WoWGlobals/namespaces/C_DelvesUI.lua
+++ RPGLootFeed_spec/_mocks/WoWGlobals/namespaces/C_DelvesUI.lua
@@ -6,5 +6,6 @@ _G.C_DelvesUI = {}
 delvesUIMocks.GetCurrentDelvesSeasonNumber = stub(_G.C_DelvesUI, "GetCurrentDelvesSeasonNumber").returns(1)
 delvesUIMocks.GetFactionForCompanion = stub(_G.C_DelvesUI, "GetFactionForCompanion").returns(2640)
 delvesUIMocks.GetCompanionInfoForActivePlayer = stub(_G.C_DelvesUI, "GetCompanionInfoForActivePlayer").returns(9)
+delvesUIMocks.GetDelvesFactionForSeason = stub(_G.C_DelvesUI, "GetDelvesFactionForSeason").returns(2683)
 
 return delvesUIMocks
