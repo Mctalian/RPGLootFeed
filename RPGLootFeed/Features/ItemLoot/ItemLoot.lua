8a76658d42ba444cf622d334545a575fbb396abb
lemieszek <dawid.lemieszek.1@gmail.com>
2025-07-20 22:56:04 +0200

    add red color for not eligible armor

--- RPGLootFeed/Features/ItemLoot/ItemLoot.lua
+++ RPGLootFeed/Features/ItemLoot/ItemLoot.lua
@@ -398,14 +398,31 @@ function ItemLoot.Element:new(info, quantity, fromLink)
 			local alwaysShowSubTypes = { Cloth = true, Leather = true, Mail = true, Plate = true }
 			local equipLocShowSubType = { INVTYPE_WEAPONMAINHAND = true, INVTYPE_WEAPONOFFHAND = true, };
 			local equipLocShowOnlySubType = { INVTYPE_WEAPON = true, INVTYPE_2HWEAPON = true }
+			local extraText = ""
 			if info.itemEquipLoc and info.itemSubType and alwaysShowSubTypes[info.itemSubType] then
-				secondaryText = secondaryText .. " [" .. _G[info.itemEquipLoc] .. " - " .. info.itemSubType .. "]"
+				extraText = " [" .. _G[info.itemEquipLoc] .. " - " .. info.itemSubType .. "]"
 			elseif info.itemEquipLoc and equipLocShowSubType[info.itemEquipLoc] and info.itemSubType then 
-				secondaryText = secondaryText .. " [" .. _G[info.itemEquipLoc] .. " - " .. info.itemSubType .. "]"
+				extraText = " [" .. _G[info.itemEquipLoc] .. " - " .. info.itemSubType .. "]"
 			elseif info.itemEquipLoc and equipLocShowOnlySubType[info.itemEquipLoc] and info.itemSubType then 
-				secondaryText = secondaryText .. " [" .. info.itemSubType .. "]"
+				extraText = " [" .. info.itemSubType .. "]"
 			elseif info.itemEquipLoc then
-				secondaryText = secondaryText .. " [" .. _G[info.itemEquipLoc] .. "]"
+				extraText = " [" .. _G[info.itemEquipLoc] .. "]"
+			end
+			if extraText then
+				if not info:IsEligibleEquipment() and info.classID == Enum.ItemClass.Armor then 
+					extraText = string.format(
+						"%s%s|r",
+						G_RLF:RGBAToHexFormat(1, 0, 0, 1),
+						extraText
+					)
+				else 
+					extraText = string.format(
+						"%s%s|r",
+						G_RLF:RGBAToHexFormat(1, 1, 1, 1),
+						extraText
+					)
+				end
+				return secondaryText .. extraText
 			end
 			return secondaryText
 		end
--- RPGLootFeed/Features/PartyLoot/PartyLoot.lua
+++ RPGLootFeed/Features/PartyLoot/PartyLoot.lua
@@ -113,14 +113,31 @@ function PartyLoot.Element:new(...)
 	local alwaysShowSubTypes = { Cloth = true, Leather = true, Mail = true, Plate = true }
 	local equipLocShowSubType = { INVTYPE_WEAPONMAINHAND = true, INVTYPE_WEAPONOFFHAND = true, };
 	local equipLocShowOnlySubType = { INVTYPE_WEAPON = true, INVTYPE_2HWEAPON = true }
+	local extraText = ""
 	if info.itemEquipLoc and info.itemSubType and alwaysShowSubTypes[info.itemSubType] then
-		element.secondaryText = element.secondaryText .. " |cffffffff[" .. _G[info.itemEquipLoc] .. " - " .. info.itemSubType .. "]|r"
+		extraText = " [" .. _G[info.itemEquipLoc] .. " - " .. info.itemSubType .. "]"
 	elseif info.itemEquipLoc and equipLocShowSubType[info.itemEquipLoc] and info.itemSubType then 
-		element.secondaryText = element.secondaryText .. " |cffffffff[" .. _G[info.itemEquipLoc] .. " - " .. info.itemSubType .. "]|r"
+		extraText = " [" .. _G[info.itemEquipLoc] .. " - " .. info.itemSubType .. "]"
 	elseif info.itemEquipLoc and equipLocShowOnlySubType[info.itemEquipLoc] and info.itemSubType then 
-		element.secondaryText = element.secondaryText .. " |cffffffff[" .. info.itemSubType .. "]|r"
+		extraText = " [" .. info.itemSubType .. "]"
 	elseif info.itemEquipLoc then
-		element.secondaryText = element.secondaryText .. " |cffffffff[" .. _G[info.itemEquipLoc] .. "]|r"
+		extraText = " [" .. _G[info.itemEquipLoc] .. "]"
+	end
+	if extraText then
+		if not info:IsEligibleEquipment() and info.classID == Enum.ItemClass.Armor then 
+			extraText = string.format(
+				"%s%s|r",
+				G_RLF:RGBAToHexFormat(1, 0, 0, 1),
+				extraText
+			)
+		else 
+			extraText = string.format(
+				"%s%s|r",
+				G_RLF:RGBAToHexFormat(1, 1, 1, 1),
+				extraText
+			)
+		end
+		element.secondaryText = element.secondaryText .. extraText
 	end
 
 	element.unitClass = select(2, UnitClass(element.unit))
